c6f25dac37fad799e92eb33a0bb96b60022477df
ColtonPaulBadock <hockey9192008@gmail.com>
2025-12-16 17:13:36 -0800

    Created item icons for all blocks, working on creating a inventory system. BACKUP DUE TO STORM!

--- Scripts/Player/ToolBelt.gd
+++ Scripts/Player/ToolBelt.gd
@@ -35,4 +35,51 @@ func _ready() -> void:
 
 #Runs every application frame.
 func _process(delta: float) -> void:
+	
+	#If the "toggle_backpack" input is pressed, flip the
+	#boolean "isInBackpack" to true if false, and false
+	#if true. This will run the inventory/backpack management
+	#system until the player hits this keybind again to
+	#close the backpack.
+	if (Input.is_action_just_pressed("toggle_backpack")):
+		isInBackpack = !isInBackpack;
+		
+		#Depending on if we where in or not in
+		#the backpack at the time for the key being pressed
+		#we will either open or exit the backpack.
+		if (isInBackpack == true):
+			openBackPack();
+		elif (isInBackpack == false):
+			closeBackPack();
+	
+	
+	#If we are suppose to be inside the backpack,
+	#then allow the player to interact with it,
+	#move stuff, etc.
+	if (isInBackpack == true):
+		runInventory();
+	
+	
+	pass;
+
+#Opens the players inventory/backpack
+#menu. The backpack will be open until
+#"closeBackPack()" is called.
+func openBackPack() -> void:
+	
+	pass;
+
+
+#Closes the players backpack/inventory
+#menu.
+func closeBackPack() -> void:
+	
+	pass;
+
+
+#Runs the inventory/backpack system for the
+#player when the menu is open. Allows for
+#moving, slecting items, etc.
+func runInventory() -> void:
+	
 	pass;
